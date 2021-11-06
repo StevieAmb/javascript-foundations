e7945511e4a02c93d3ad9377c895ef55fb7a6c58
Stevie Ambroise <s.ambroise@protonmail.com>
2021-11-06 14:33:00 -0600

    Add material test and suite

--- crafting/src/material.js
+++ crafting/src/material.js
@@ -1,5 +1,24 @@
 class Material {
+ constructor(cloth, price, amount, units) {
+  this.name = cloth;
+  this.price = price;
+  this.amount = amount
+  this.units = units;
+ }
 
+ useMaterial(amtNeeded) {
+   if (this.amount < amtNeeded) {
+     return `You don\'t have enough ${this.name}! Try using ${this.amount} ${this.units} or less.`;
+   } else {
+    this.amount = this.amount - amtNeeded;
+    return `You now have ${this.amount} ${this.units} of ${this.name} left.`;
+   }
+ }
+
+ calculateMaterialCost() {
+   return this.price * this.amount;
+ }
 }
+
 
 module.exports = Material;
