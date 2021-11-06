07064cdf938e72b43f7f7ded332aa8b514a5d945
Stevie Ambroise <s.ambroise@protonmail.com>
2021-11-06 16:31:56 -0600

    Add crafting class to class suite

--- crafting/src/craft.js
+++ crafting/src/craft.js
@@ -1,4 +1,23 @@
+var Material = require("../src/material.js");
+
 class Craft {
+ constructor(craft) {
+   this.name = craft.type;
+   this.materials = craft.materials;
+   this.completed = false;
+ }
+ completeCraft() {
+   this.completed = true;
+   return `All done! It looks great!`
+ }
+ calculateProjectTotal() {
+   var sum = 0;
+   for (var i = 0; i < this.materials.length; i++) {
+     sum += this.materials[i].calculateMaterialCost();
+   }
+   return sum;
+ }
+
 
 }
 
