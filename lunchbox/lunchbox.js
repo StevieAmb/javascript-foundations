5a41f419c41878738a0492a66a10fce82738bcea
Stevie Ambroise <s.ambroise@protonmail.com>
2021-11-08 21:48:15 -0700

    Add snack and lunchbox test suites

--- lunchbox/lunchbox.js
+++ lunchbox/lunchbox.js
@@ -1,5 +1,34 @@
 class LunchBox {
+  constructor(lunchbox) {
+    this.owner = lunchbox.owner;
+    this.material = lunchbox.madeOf;
+    this.color = lunchbox.color;
+    this.shape = lunchbox.shape;
+    this.snacks = [];
+    this.healthySnack = [];
+  }
 
+  acquireSnack(fruitSnack) {
+    this.snacks.push(fruitSnack);
+    fruitSnack.isInLunchBox = true;
+
+    if (fruitSnack.type.includes('fruit') ||          fruitSnack.type.includes('Fruit')) {
+      this.healthySnack.push(fruitSnack.type);
+    }
+ }
+
+  findHealthySnacks() {
+     return this.healthySnack;
+  }
 }
+
+//so, there's a function that checks whether
+//a snack has the word fruit in it. If there's
+//fruit in it, it reads true. Returns true is has
+//fruit in it.
+
+//this.type is the one that has the snack name.
+
+//Goal: to get to identifying all the healthy snacks, so all the snacks that will return true if they have fruit in it.
 
 module.exports = LunchBox;
