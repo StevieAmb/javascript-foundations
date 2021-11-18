6e2fde4f2ae4f0eb509e88a8783190b24292dc3f
Stevie Ambroise <s.ambroise@protonmail.com>
2021-11-18 11:54:47 -0700

    Add being and being test for spaceport

--- spaceport/src/being.js
+++ spaceport/src/being.js
@@ -1,4 +1,14 @@
 class Being {
+  constructor(being, species) {
+    this.name = being;
+    this.species = species;
+    this.credits = 0;
+    this.isAlive = true;
+  }
+
+  updateCredits(addedCredits) {
+    this.credits += addedCredits;
+  }
 }
 
 module.exports = Being;
